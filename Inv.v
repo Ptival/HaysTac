952fbb0c088070ec2cd58c873ca9a77f28d628c7
Valentin Robert <valentin.robert.42@gmail.com>
2017-04-13 17:07:15 -0700

    inv_clear_subst does not fail on clear

--- Inv.v
+++ Inv.v
@@ -4,4 +4,7 @@ From HaysTac Require Import
 
 Ltac inv_clear H := inversion H; clear H.
 
-Ltac inv_clear_subst H := inversion H; clear H; subst_all.
+Ltac inv_clear_subst H :=
+  inversion H;
+  try (clear H); (* sometimes H can't be cleared because it's referred to *)
+  subst_all.
