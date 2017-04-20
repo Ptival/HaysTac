e12e3677a60000c0403f41464d8a31c6c94ceb6c
Valentin Robert <valentin.robert.42@gmail.com>
2017-04-20 14:24:21 -0700

    changed on to use with

--- Core/On.v
+++ Core/On.v
@@ -4,6 +4,7 @@ From HaysTac.Core Require Import
      Enumerate
      Find
      Get
+     With
 .
 
 Local Ltac dbg flag tactic H :=
@@ -12,77 +13,54 @@ Local Ltac dbg flag tactic H :=
   | false => tactic H
   end.
 
+(** [on (t1, ..., tn) tac] finds a hypothesis [H] whose type mentions
+    all the terms [t1], ..., [tn], and runs [tac H].  [on'] is the
+    same as [on] but it outputs what hypothesis was used, for
+    debugging purposes. *)
 Local Ltac _on flag tuple tactic :=
   let H := find_hyp_mentioning_all tuple in
   dbg flag tactic H.
-
-(** [on (t1, ..., tn) tac] finds a hypothesis [H] whose type mentions
-    all the terms [t1], ..., [tn], and runs [tac H]. *)
-Ltac on := _on false.
-
-(** [on'] is the same as [on] but it outputs what hypothesis was used,
-    for debugging purposes. *)
+Ltac on  := _on false.
 Ltac on' := _on true.
 
-Local Ltac _on2 flag tuple tactic :=
-  let H1 := find_hyp_mentioning_all tuple in
-  let H2 := find_hyp_mentioning_all tuple in
-  assert_distinct H1 H2; dbg flag tactic H1 H2.
-
-Ltac on2 := _on2 false.
-
+Local Ltac _on2 flag tuple tactic := with_2_hyps ltac:(dbg flag tactic).
+Ltac on2  := _on2 false.
 Ltac on2' := _on2 true.
 
-Local Ltac _on3 flag tuple tactic :=
-  let H1 := find_hyp_mentioning_all tuple in
-  let H2 := find_hyp_mentioning_all tuple in
-  let H3 := find_hyp_mentioning_all tuple in
-  assert_distinct H1 H2;
-  assert_distinct H2 H3;
-  assert_distinct H1 H3;
-  dbg flag tactic H1 H2 H3.
-
-Ltac on3 := _on3 false.
-
+Local Ltac _on3 flag tuple tactic := with_3_hyps ltac:(dbg flag tactic).
+Ltac on3  := _on3 false.
 Ltac on3' := _on3 true.
 
-Local Ltac _on_head flag type tactic :=
-  let H := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H) type;
-  dbg flag tactic H.
-
 (** [on_head type tac] finds a hypothesis [H] whose type starts with
-    [type] and runs [tac H]. *)
+    [type] and runs [tac H].  [on_head'] is the same as [on_head] but
+    it outputs what hypothesis was used, for debugging purposes. *)
+Local Ltac _on_head flag type tactic :=
+  with_hyp ltac:(
+    fun H =>
+      assert_same ltac:(get_head_hyp H) type;
+      dbg flag tactic H
+  ).
 Ltac on_head := _on_head false.
-
-(** [on_head'] is the same as [on_head] but it outputs what hypothesis
-    was used, for debugging purposes. *)
 Ltac on_head' := _on_head true.
 
+
 Local Ltac _on_head2 flag type tactic :=
-  let H1 := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H1) type;
-  let H2 := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H2) type;
-  assert_distinct H1 H2;
-  dbg flag tactic H1 H2.
-
+  with_2_hyps ltac:(
+    fun H1 H2 =>
+      assert_same ltac:(get_head_hyp H1) type;
+      assert_same ltac:(get_head_hyp H2) type;
+      dbg flag tactic H1 H2
+  ).
 Ltac on_head2 := _on_head2 false.
-
 Ltac on_head2' := _on_head2 true.
 
 Local Ltac _on_head3 flag type tactic :=
-  let H1 := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H1) type;
-  let H2 := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H2) type;
-  let H3 := enumerate_hyps in
-  assert_same ltac:(get_head_hyp H3) type;
-  assert_distinct H1 H2;
-  assert_distinct H2 H3;
-  assert_distinct H1 H3;
-  dbg flag tactic H1 H2 H3.
-
+  with_3_hyps ltac:(
+    fun H1 H2 H3 =>
+      assert_same ltac:(get_head_hyp H1) type;
+      assert_same ltac:(get_head_hyp H2) type;
+      assert_same ltac:(get_head_hyp H3) type;
+      dbg flag tactic
+  ).
 Ltac on_head3 := _on_head3 false.
-
 Ltac on_head3' := _on_head3 true.
--- Core/With.v
+++ Core/With.v
@@ -9,10 +9,21 @@ Ltac with_hyp t :=
   let H := enumerate_hyps in
   t H.
 
-(** [with_2_hyps t] enumerates pairs of distinct hypotheses [H1] and
-    [H2] and succeeds on the first success of [t H1 H2]. *)
+(** [with_2_hyps t] enumerates pairs of distinct hypotheses [H1] [H2]
+    and succeeds on the first success of [t H1 H2]. *)
 Ltac with_2_hyps t :=
   let H1 := enumerate_hyps in
   let H2 := enumerate_hyps in
   assert_distinct H1 H2;
   t H1 H2.
+
+(** [with_3_hyps t] enumerates triples of distinct hypotheses [H1]
+    [H2] [H3] and succeeds on the first success of [t H1 H2 H3]. *)
+Ltac with_3_hyps t :=
+  let H1 := enumerate_hyps in
+  let H2 := enumerate_hyps in
+  let H3 := enumerate_hyps in
+  assert_distinct H1 H2;
+  assert_distinct H1 H3;
+  assert_distinct H2 H3;
+  t H1 H2 H3.
